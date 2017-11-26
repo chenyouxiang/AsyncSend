f670eec0ae8fc95ad5bb8252e0ca54d3ed86cdd9
chenyouxiang <chenyouxiang007@qq.com>
2017-11-26 12:29:03 +0800

    commit

--- src/base/ConnectionBase.h
+++ src/base/ConnectionBase.h
@@ -1,17 +1,17 @@
 #ifndef CONNECTION_BASE_H
 #define CONNECTION_BASE_H
 namespace AsyncSend{
-	class ConnectionBase {
-	public:
-		virtual ~ConnectionBase(){
+    class ConnectionBase {
+    public:
+        virtual ~ConnectionBase(){
 
-		};
-		virtual void handle_write_event()=0;
-    	virtual void handle_read_event()=0;
-    	friend class Poller;
+        };
+        virtual void handle_write_event()=0;
+        virtual void handle_read_event()=0;
+        friend class Poller;
     private:
-    	virtual int get_fd()=0;
-    	virtual void close_connection()=0;
-	};
+        virtual int get_fd()=0;
+        virtual void close_connection()=0;
+    };
 }
 #endif
