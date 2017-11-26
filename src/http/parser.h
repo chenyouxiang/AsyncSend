4b1c54299eb2d509c5c53549fe2ac7398ecfe483
chenyouxiang <chenyouxiang007@qq.com>
2017-11-26 12:29:54 +0800

    commit

--- src/http/parser.h
+++ src/http/parser.h
@@ -9,34 +9,34 @@ namespace http
 {
 class parser{
 public:
-	parser(Buffer* buf);
-	void execute();
-	void reset(Buffer* buf){
-		_buf = buf;
-		_length = 0;
-		_is_complete = false;
-	}
-	size_t get_length(){
-		return _length;
-	}
+    parser(Buffer* buf);
+    void execute();
+    void reset(Buffer* buf){
+        _buf = buf;
+        _length = 0;
+        _is_complete = false;
+    }
+    size_t get_length(){
+        return _length;
+    }
 
-	bool is_complete(){
-		return _is_complete;
-	}
+    bool is_complete(){
+        return _is_complete;
+    }
 
-	std::string& get_body(){
-		return _body;
-	}
+    std::string& get_body(){
+        return _body;
+    }
 private:
-	static int on_body_complete(http_parser* p, const char *at, size_t length);
-	static int on_headers_complete(http_parser* p, const char *at, size_t length);
-	static int on_message_end(http_parser* p);
-	Buffer* _buf;
-	std::string _body;
-	size_t _length;
-	http_parser _parser;
-	http_parser_settings _option;
-	bool _is_complete;
+    static int on_body_complete(http_parser* p, const char *at, size_t length);
+    static int on_headers_complete(http_parser* p, const char *at, size_t length);
+    static int on_message_end(http_parser* p);
+    Buffer* _buf;
+    std::string _body;
+    size_t _length;
+    http_parser _parser;
+    http_parser_settings _option;
+    bool _is_complete;
 };
 }
 }
